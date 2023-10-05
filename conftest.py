08fca3ae517978b1cb3b3bd3c5a7c42f0bd63e86
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2023-10-05 22:38:35 +0300

    Initial commit

---
+++ conftest.py
@@ -0,0 +1,61 @@
+import  pytest
+import yaml
+
+with open("testdata.yaml") as f:
+    testdata = yaml.safe_load(f)
+
+user = testdata["username"]
+passwd = testdata["password"]
+
+@pytest.fixture()
+def x_selector_1():
+    return """//*[@id="login"]/div[1]/label/input"""
+
+@pytest.fixture()
+def x_selector_2():
+    return """//*[@id="login"]/div[2]/label/input"""
+
+@pytest.fixture()
+def x_selector_3():
+    return """//*[@id="app"]/main/div/div/div[2]/h2"""
+
+@pytest.fixture()
+def btn_selector():
+     return "button"
+
+@pytest.fixture()
+def expected_result_1():
+     return "401"
+
+@pytest.fixture()
+def x_selector_4():
+    return """//*[@id="app"]/main/nav/ul/li[3]/a"""
+
+@pytest.fixture()
+def expected_result_2():
+    return f"Hello, {user}"
+
+@pytest.fixture()
+def x_selector_home1():
+    return """//*[@id="create-btn"]"""
+
+@pytest.fixture()
+def title_home1():
+    return """//*[@id="create-item"]/div/div/div[1]/div/label/input"""
+
+@pytest.fixture()
+def description_home1():
+    return """//*[@id="create-item"]/div/div/div[2]/div/label/span/textarea"""
+
+@pytest.fixture()
+def content_home1():
+    return """//*[@id="create-item"]/div/div/div[3]/div/label/span/textarea"""
+
+@pytest.fixture()
+def btn_home():
+    return """//*[@id="create-item"]/div/div/div[7]/div/button/span"""
+
+
+@pytest.fixture()
+def expected_result_3():
+     return """//*[@id="app"]/main/div/div[1]/h1"""
---
+++ test.py
@@ -0,0 +1,44 @@
+import yaml
+from module import Site
+import pytest
+
+with open("testdata.yaml") as f:
+    testdata = yaml.safe_load(f)
+
+site = Site(testdata["address"])
+user = testdata["username"]
+passwd = testdata["password"]
+txt = "Создан автом"
+
+
+def test_step1(x_selector_1, x_selector_2, btn_selector, x_selector_home1, title_home1, description_home1,
+               content_home1, btn_home, expected_result_3):
+    input1 = site.find_element("xpath", x_selector_1)
+    input1.clear()
+    input1.send_keys(user)
+    input2 = site.find_element("xpath", x_selector_2)
+    input2.clear()
+    input2.send_keys(passwd)
+    btn = site.find_element("css", btn_selector)
+    btn.click()
+
+    btn2 = site.find_element("xpath", x_selector_home1)
+    btn2.click()
+
+    input3 = site.find_element("xpath", title_home1)
+    input3.send_keys(txt)
+    site.pause()
+
+    input4 = site.find_element("xpath", description_home1)
+    input4.send_keys(txt)
+    site.pause()
+
+    input5 = site.find_element("xpath", content_home1)
+    input5.send_keys(txt)
+    site.pause()
+
+    btn3 = site.find_element("xpath", btn_home)
+    btn3.click()
+    site.pause()
+    text_post = site.find_element("xpath", expected_result_3)
+    assert text_post.text == txt, "test1 failed"
